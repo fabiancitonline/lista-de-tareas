94ac78ad3fa32468e5ebaad254f970d1d27d727c
Fabian Bariandaran <fbariandaran@consultoriaglobal.com.ar>
2022-02-24 02:09:52 -0300

    subida de app

--- lista-de-tareas/src/app/app.component.ts
+++ lista-de-tareas/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lista-de-tareas';
+  public items: string[] = [];
+
+  public NuevaTarea = '';
+
+  public AddTarea() {
+    if (this.NuevaTarea === '') {
+    } else {
+      this.items.push(this.NuevaTarea);
+      this.NuevaTarea = '';
+    }
+  }
+
+  public borrarTarea(index: number) {
+    this.items.splice(index, 1);
+  }
 }
